d62f8390b765129bd69e653f4bfba331b37590ab
Nicole-Leiva <42221903+Nicole-Leiva@users.noreply.github.com>
2022-10-17 20:59:07 -0300

    database pass restore

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -5,7 +5,7 @@ if (!conn) {
   conn = new Pool({
     user: "postgres",
     database: "is2_proyecto",
-    password: "12345678",
+    password: "12345",
     host: "localhost",
     port: 5432,
   });
